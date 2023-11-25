1ccc2c1ba96d2947db4feea949ede012e6d64d6a
Bustanil Arifin <bustanil@protonmail.com>
2023-11-25 23:42:45 +0800

    lspconfig, treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,39 @@
--- show line number
-vim.opt.number = true
--- activate mouse
-vim.opt.mouse = 'a'
+vim.g.mapleader = ' ' -- leader key
 
--- leader key
-vim.g.mapleader = ' '
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.number = true -- show line number
+vim.opt.mouse = 'a' -- activate mouse
+vim.opt.title = true 
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
+vim.opt.splitkeep = "cursor"
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
 
 
 -- lazy.vim package manager
@@ -32,6 +61,42 @@ require("lazy").setup({
 	},
 	{ "nvim-telescope/telescope.nvim",
 	  tag = "0.1.4",
-	  dependencies = { "nvim-lua/plenary.nvim" }
+	  dependencies = { "nvim-lua/plenary.nvim" },
+	  config = function()
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+	  end,
 	},
+  { "smjonas/inc-rename.nvim",
+    config = function()
+      require("inc_rename").setup()
+    end,
+  },
+  { "neovim/nvim-lspconfig" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }
 })
+
+
+-- LSP
+require'lspconfig'.tsserver.setup{}
+-- vim.lsp.start({
+--  name = 'typescript',
+--  cmd = {'typescript-language-server', '--stdio'},
+--  root_dir = vim.fs.dirname(vim.fs.find({'package.json'}, { upward = true })[1]),
+--})
