f3ba7f7d75c6ee7f34c11c1e24e9c52ca6d8f8f3
Bustanil Arifin <bustanil.arifin@grabtaxi.com>
2023-11-26 23:12:46 +0800

    add autocomplete, scala metals

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -91,6 +91,10 @@ require("lazy").setup({
       "hrsh7th/cmp-path", 
       "hrsh7th/cmp-cmdline",
       "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-nvim-lsp",
+      "rafamadriz/friendly-snippets",
     }
   },
   -- color scheme
@@ -108,8 +112,65 @@ require("lazy").setup({
 })
 
 
+-- autocomplete config taken from kickstart.nvim
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+
+require('luasnip.loaders.from_vscode').lazy_load()
+luasnip.config.setup {}
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },    
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- LSP
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.hls.setup{}
--- TODO scala metals
+local lspconfig = require('lspconfig')
+
+lspconfig.tsserver.setup { capabilities = capabilities, }
+lspconfig.hls.setup{ capabilities = capabilities, }
+lspconfig.metals.setup{ capabilities = capabilities, }
+lspconfig.phpactor.setup{ capabilities = capabilities, }
 -- TODO PHP
+
